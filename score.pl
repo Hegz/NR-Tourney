37eb837c126439f4d13013be91eba43a8e9667e3
Adam Fairbrother <adam.fairbrother@gmail.com>
2014-07-26 23:00:13 -0700

    Added in proper score options

--- score.pl
+++ score.pl
@@ -33,8 +33,6 @@ my $term = Term::ReadLine->new('brand');
 my $player_data = LoadFile($data_file);
 delete $player_data->{BYE};
 
-my @score_options = ('Won both games', 'Won and Lost', 'Win and Timeout', 'Lost and Timeout', 'Timeout first game', 'Lost both games');
-
 # Main Menu
 my @menu = ('Add Score Data', 'Set current round','Ready next round', 'View standings', 'Save & Quit');
 my $main_menu;
@@ -50,7 +48,6 @@ do {
 		Score_Data() when $_ eq $menu[0];
 		Select_round() when $_ eq $menu[1];
 	}
-
 
 } while ($main_menu ne $menu[4]); 
 DumpFile("playerdata.yml", $player_data);
@@ -69,9 +66,14 @@ sub Score_Data {
 	# Filter out players with score data for this round
 	my @sorted_players;  ;
 	for (sort keys %$player_data){
-		if ($player_data->{$_}->{prestige}[$score_round] <= 0) {
+		unless (defined $player_data->{$_}->{prestige}[$score_round]) {
 			push @sorted_players, $_;
 		}
+	}
+
+	if ( scalar @sorted_players  == 0 ){
+		print "No More players to score for round " . ($score_round + 1) . ".\n";
+		return;
 	}
 
 	# prompt for player
@@ -79,54 +81,95 @@ sub Score_Data {
 		prompt => 'Score which player?',
 		choices => \@sorted_players,
 	);
+	my $Player2 = $player_data->{$player_data->{$Player}->{opponents}[$score_round]};
+
+	print "Scoring game vs " . $Player2 . "\n"; 
+
+# valid scoring options as of Aug, 2014
+	my @score_options = (
+		'Won both games', 
+		'Won and Lost', 
+		'Won Game 1 and Timeout w/ AP lead', 
+		'Won Game 1 and Timeout w/ AP Tie', 
+		'Won Game 1 and Timeout w/ AP Deficit', 
+		'Lost Game 1 and Timeout w/ AP Lead', 
+		'Lost Game 1 and Timeout w/ AP Tie', 
+		'Lost Game 1 and Timeout w/ AP Deficit', 
+		'Timeout first game w/ AP Lead', 
+		'Timeout first game w/ AP Tie', 
+		'Timeout first game w/ AP Deficit', 
+		'Lost both games'
+	);
 
 	# Prompt for Score
 	my $score = $term->get_reply (
 		prompt => 'Match Result',
 		choices => \@score_options,
 	);
+	
+
 
 	for ($score) {
 		no warnings qw(experimental);
 		when ($score eq $score_options[0]) {
+		# Won both Games
 			$player_data->{$Player}->{prestige}[$score_round] = 4;
-			$player_data->{$player_data->{$Player}->{opponents}[$score_round]}->{prestige}[$score_round] = 0;
+			$player_data->{$Player2}->{prestige}[$score_round] = 0;
 		}
 		when ($score eq $score_options[1]) {
+		# Split games
 			$player_data->{$Player}->{prestige}[$score_round] = 2;
-			$player_data->{$player_data->{$Player}->{opponents}[$score_round]}->{prestige}[$score_round] = 2;
+			$player_data->{$Player2}->{prestige}[$score_round] = 2;
 		}
 		when ($score eq $score_options[2]) {
+		# Won Game 1 and timeout Game 2 with agenda point lead.
 			$player_data->{$Player}->{prestige}[$score_round] = 3;
-			$player_data->{$player_data->{$Player}->{opponents}[$score_round]}->{prestige}[$score_round] = 1;
+			$player_data->{$Player2}->{prestige}[$score_round] = 0;
 		}
 		when ($score eq $score_options[3]) {
-			$player_data->{$Player}->{prestige}[$score_round] = 1;
-			$player_data->{$player_data->{$Player}->{opponents}[$score_round]}->{prestige}[$score_round] = 3;
+		# Won Game 1 and timeout Game 2 with agenda point Tie
+			$player_data->{$Player}->{prestige}[$score_round] = 3;
+			$player_data->{$Player2}->{prestige}[$score_round] = 1;
 		}
 		when ($score eq $score_options[4]) {
-			$player_data->{$Player}->{prestige}[$score_round] = 1;
-			$player_data->{$player_data->{$Player}->{opponents}[$score_round]}->{prestige}[$score_round] = 1;
+		# Won Game 1 and timeout Game 2 with agenda point deficit
+			$player_data->{$Player}->{prestige}[$score_round] = 2;
+			$player_data->{$Player2}->{prestige}[$score_round] = 1;
 		}
 		when ($score eq $score_options[5]) {
+		# Lost game and timeout with Agenda point lead
+			$player_data->{$Player}->{prestige}[$score_round] = 1;
+			$player_data->{$Player2}->{prestige}[$score_round] = 2;
+		}
+		when ($score eq $score_options[6]) {
+		# Lost game and timeout with Agenda point Tie
+			$player_data->{$Player}->{prestige}[$score_round] = 1;
+			$player_data->{$Player2}->{prestige}[$score_round] = 3;
+		}
+		when ($score eq $score_options[7]) {
+		# Lost Game 1 and timeout Game 2 with agenda point deficit
 			$player_data->{$Player}->{prestige}[$score_round] = 0;
-			$player_data->{$player_data->{$Player}->{opponents}[$score_round]}->{prestige}[$score_round] = 4;
+			$player_data->{$Player2}->{prestige}[$score_round] = 3;
+		}
+		when ($score eq $score_options[8]) {
+		# Timeout Game 1 with agenda point lead.
+			$player_data->{$Player}->{prestige}[$score_round] = 1;
+			$player_data->{$Player2}->{prestige}[$score_round] = 0;
+		}
+		when ($score eq $score_options[9]) {
+		# Timeout Game 1 with agenda point tie.
+			$player_data->{$Player}->{prestige}[$score_round] = 1;
+			$player_data->{$Player2}->{prestige}[$score_round] = 1;
+		}
+		when ($score eq $score_options[10]) {
+		# Timeout Game 1 with agenda point deficit
+			$player_data->{$Player}->{prestige}[$score_round] = 0;
+			$player_data->{$Player2}->{prestige}[$score_round] = 1;
+		}
+		when ($score eq $score_options[11]) {
+		# Lost both Games
+			$player_data->{$Player}->{prestige}[$score_round] = 0;
+			$player_data->{$Player2}->{prestige}[$score_round] = 4;
 		}
 	}
 }
-
-
-exit 0;
-# Print player with no score data for this round
-
-# Select a player
-
-
-# @score_options = ('Won both games', 'Won and Lost', 'Win and Timeout', 'Lost and Timeout', 'Timeout first game', 'Lost both games');
-# Select Score Options
-# 4 Points -- 2 Wins
-# 2 Points -- 1 Win & 1 Loss
-# 3 Points -- 1 Win & timeout
-# 1 Point  -- 1 Loss & timeout
-# 1 Point  -- 2 timeouts
-# 0 points -- 2 Loses
